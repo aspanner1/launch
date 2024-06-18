d2c03c723dc27b736f79e73fe96d4f3296522cfe
Arin Spanner <arinspanner1@gmail.com>
2024-06-18 14:48:59 -0700

    aa

--- py130/exercises/easy_2/2.py
+++ py130/exercises/easy_2/2.py
@@ -1,7 +1,11 @@
+""" 
 def create_user(username, *, email, age):
     return {username=, }
+
 
 print(create_user("Srdjan", email="srdjan@example.com", age=39))
 # {"username": "Srdjan", "email": "srdjan@example.com", "age": 39}
 print(create_user("Srdjan", "srdjan@example.com", age=39))
 # Raises an exception
+
+"""
